82381239cbfa052054351ca2d0b9698a2ac8de81
Vinicius Santos <viniciusmain1@gmail.com>
2021-12-04 19:31:23 -0300

    Correção e adição de arquivos

---
+++ config.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+    port: process.env.PORT,
+    host: process.env.HOST,
+    firebaseConfig: {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+    }
+}
